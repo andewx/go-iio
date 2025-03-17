006a41a6fa425aec82b63980f498ecb9d402a363
andewx <brian.michael.anderson@gmail.com>
2025-03-16 20:29:53 -0700

    Added FFT Frames Overlap

--- main/main.go
+++ main/main.go
@@ -3,11 +3,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
+	"time"
 
+	"github.com/andewx/fft"
 	"github.com/andewx/go-iio/common"
-	"github.com/andewx/go-iio/fft"
 	"github.com/andewx/go-iio/iio"
 	"github.com/andewx/go-iio/plot"
 	"github.com/andewx/go-iio/sdr"
@@ -20,8 +22,10 @@ func idxCheck(args []string, index int) string {
 	return ""
 }
 
+var ready bool
+
 func main() {
-	var g *sdr.SDR
+	var device *sdr.SDR
 	var args []string
 	var err error
 	var host, name string
@@ -29,9 +33,14 @@ func main() {
 	name = "ad9361"
 	var scan *iio.ScanBlock
 	var attrval, attrname bool
+	var kill chan int
+	var streamRx chan int
 
 	attrval = false
 	attrname = false
+	ready = true
+	kill = make(chan int, 1)
+	streamRx = make(chan int, 1)
 
 	// Get OS Args
 	args = os.Args[1:]
@@ -89,21 +98,72 @@ func main() {
 	fmt.Printf("...Scanning for available contexts %s\n", scan.String())
 	fmt.Printf("...Testing device availability for AD9361 %s\n", iio.Version())
 
-	g, err = sdr.ConnectNetwork(host, name)
+	device, err = sdr.NewSDR(host, name, 32678)
 
-	if err != nil || g == nil {
+	if err != nil || device == nil {
 		fmt.Printf("Unable to connect to Network Device %s at %s \n", host, name)
 		return
 	}
 
-	g.Print(console)
-
-	// Configure Device and Read In Device Stream and Plot
-	iqData := g.GetSamples(1024, 2)
-	spectrum, _ := fft.FFT64(iqData)
-	grapher := plot.NewFFTPlotServer(g, spectrum)
+	device.Print(console)
 	fmt.Printf("Starting FFT Plot Server\n")
 	fmt.Printf("...Listening on locahost:8080\n")
-	grapher.Start()
+	fmt.Printf("Press [enter] to exit...\n")
 
+	// Configure Device and Read In Device Stream and Plot at 10FPS
+	spectrum := make([]complex64, device.Params.BufferSize/2)
+	go device.StreamRx(40, streamRx)
+	fft.FFTSinglePrecision(spectrum)
+	grapher := plot.NewFFTPlotServer(device, 4096)
+	go grapher.Start(kill)
+	go ListenInput(kill)
+
+	// Start a 10 frames per second 1oop
+	frameTime := int64((1.0 / float64(10)) * 1e9)
+	for ready {
+		timeSet := time.Now().UnixNano()
+		timeDelta := time.Now().UnixNano() - timeSet
+		for timeDelta < frameTime {
+			// Sleep this thread until the next frame if this frame is not ready
+			// Sleep Again by a delta amount
+			time.Sleep(time.Nanosecond * time.Duration(timeDelta))
+			timeDelta = time.Now().UnixNano() - timeSet
+		}
+
+		samples := device.GetStreamRxData(streamRx)
+		err = fft.FFTSinglePrecision(samples)
+
+		if err != nil {
+			fmt.Printf("Error in FFT\n")
+			kill <- 1
+			ready = false
+		}
+
+		data := fft.PowerSpectrum(samples)
+
+		grapher.SetReal(data)
+	}
+
+}
+
+// ListenInput is a function that kills the process by setting the global var ready to false
+func ListenInput(signal chan int) {
+	reader := bufio.NewReader(os.Stdin)
+	reader.ReadString('\n')
+	//Send message to kill
+	signal <- 1
+	ready = false
+	fmt.Printf("Exiting device...\n")
+}
+
+// ComplexToFloat32 converts a complex64 array to a float32 array
+func ComplexToFloat32(in []complex64) [][]float32 {
+	out := make([][]float32, 2)
+	out[0] = make([]float32, len(in))
+	out[1] = make([]float32, len(in))
+	for i, v := range in {
+		out[0][i] = real(v)
+		out[1][i] = imag(v)
+	}
+	return out
 }
